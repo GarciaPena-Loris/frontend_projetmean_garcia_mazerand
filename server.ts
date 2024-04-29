2a130dfc3b4e9fb3d0632aa3399463978ac53bf0
GarciaPena-Loris <loris.garcia-pena@etu.umontpellier.fr>
2024-04-29 23:58:28 +0200

    modif procile'

--- server.ts
+++ server.ts
@@ -1,8 +1,9 @@
-const express = require('express');
-const path = require('path');
+import express, { Request, Response } from 'express';
+import path from 'path';
+
 const app = express();
 app.use(express.static(__dirname + '/dist/frontend-projetmean-garcia-mazerand'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/frontend-projetmean-garcia-mazerand/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.get('/*', function(req: Request, res: Response) {
+  res.sendFile(path.join(__dirname + '/dist/frontend-projetmean-garcia-mazerand/index.html'));
+});
+app.listen(process.env['PORT'] || 8080);
