625efdcc3a6ddc6b0c5701c0f13982fbde67bffb
GarciaPena-Loris <loris.garcia-pena@etu.umontpellier.fr>
2024-04-30 09:47:04 +0200

    ajout front

--- server.js
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from 'express';
-import path from 'path';
-
-const app = express();
-app.use(express.static(__dirname + '/dist/frontend_projetmean_garcia_mazerand/browser'));
-app.get('/*', function(req: Request, res: Response) {
-  res.sendFile(path.join(__dirname + '/dist/frontend_projetmean_garcia-mazerand/browser/index.html'));
-});
-app.listen(process.env['PORT'] || 8080);
